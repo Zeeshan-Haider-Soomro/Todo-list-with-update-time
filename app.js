addcfc286fe9666f7224abcd6bd22b552815ce2e
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-19 04:27:15 +0500

    done by zeeshan

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ function taskGenerate() {
 
   let date = new Date();
 
-  task.innerText += ` ${userInput.value} \n`;
-  spani.innerText += ` ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}  \n `;
-  userInput.value = "";
+  if(userInput.value.trim() == ""){
+
+    alert("you must write something")
+  }
+  else{
+    task.innerText += `✅ ${userInput.value} \n `;
+    spani.innerText += `📌 ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}  \n `;
+    userInput.value = "";
+  }
+
+  
 }
