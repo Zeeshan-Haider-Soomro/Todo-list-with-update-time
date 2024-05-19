0e0334ec20028213d7488ba3ff387a148f437860
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-19 05:47:42 +0500

    done by zeeshan haider soomro

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+
+let date = new Date();
+let yearChanger = document.querySelector("#yearChanger")
+yearChanger.innerText = date.getFullYear();
+
 function taskGenerate() {
   let userInput = document.querySelector("#userInput");
   let task = document.querySelector("#task");
   let spani = document.querySelector("#spani");
-
-  let date = new Date();
+  let error = document.querySelector("#error");
 
   if(userInput.value.trim() == ""){
-
-    alert("you must write something")
+    error.innerText = "you must write something !!"
+    // alert("you must write something")
   }
   else{
     task.innerText += `✅ ${userInput.value} \n `;
     spani.innerText += `📌 ${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}  \n `;
     userInput.value = "";
+    error.innerText = ""
   }
-
-  
 }
+
